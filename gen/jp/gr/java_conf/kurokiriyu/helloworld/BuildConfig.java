66e76f6a1784954b7890841c20575a725d4c7011
kurokiriyu <noritaka.takeda1983@gmail.com>
2012-07-31 22:22:45 +0900

    ハローワールド

---
+++ gen/jp/gr/java_conf/kurokiriyu/helloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.gr.java_conf.kurokiriyu.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
